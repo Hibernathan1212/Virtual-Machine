1c13c7b7e3070099c80aaaa45c473673789ccd6c
Nathan <nnthurber@icloud.com>
2024-09-17 14:21:41 +0700

    Initial Commit

---
+++ Virtual Machine/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  Virtual Machine
+//
+//  Created by Nathan Thurber on 17/9/24.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
