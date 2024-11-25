2457f839828a971a7a6afa7cc72f28f59ad0194a
Nathan <nnthurber@icloud.com>
2024-11-25 20:57:58 +0700

    first commit

--- Virtual Machine/main.c
+++ Virtual Machine/main.c
@@ -6,9 +6,89 @@
 //
 
 #include <stdio.h>
+#include <stdbool.h>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
+#define sp (registers[SP])
+#define ip (registers[IP])
+
+typedef enum {
+    PSH,
+    ADD,
+    POP,
+    SET,
+    MOV,
+    HLT
+} InstructionSet;
+
+typedef enum {
+    A, B, C, D, E, F, IP, SP, //program counter, stack counter
+    NUM_OF_REGISTERS
+} Registers;
+
+const int program[] = {
+    PSH, 5,
+    PSH, 6,
+    ADD,
+    POP,
+    //MOV,
+    //SET,
+    HLT
+};
+
+int registers[NUM_OF_REGISTERS];
+
+int stack[256];
+
+bool running = 1;
+
+void eval(int instr) 
+{
+    switch (instr) 
+    {
+        case HLT:
+        {
+            running = false;
+            break;
+        }
+        case PSH:
+        {
+            sp++;
+            stack[sp] = program[++ip];
+            break;
+        }
+        case POP:
+        {
+            int val_popped = stack[sp--];
+            printf("popped %d\n", val_popped);
+            break;
+        }
+        case ADD: 
+        {
+            int a = stack[sp--];
+            int b = stack[sp--];
+            int result = b + a;
+            
+            sp++;
+            stack[sp] = result;
+            break;
+        }
+        case SET:
+        {
+            
+        }
+    }
+}
+
+int main(int argc, const char * argv[])
+{
+    sp = -1;
+    ip = 0;
+    
+    while (running)
+    {
+        eval(program[ip]);
+        ip++;
+    }
+    
     return 0;
 }
